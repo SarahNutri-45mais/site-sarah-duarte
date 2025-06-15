017903c6214c602973eb3107bf5abefcd965c761
SarahNutri-45mais <sarahduarte.nut@gmail.com>
2025-06-15 14:21:07 -0300

    Update tailwind.config.js

--- src/pages/tailwind.config.js
+++ src/pages/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: { extend: {} },
   plugins: [],
 };
